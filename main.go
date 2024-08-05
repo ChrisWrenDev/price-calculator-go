940101c48b5fd1784cdc13bdc831b7cab11e0983
Chris Wren <chris.wren@bt.com>
2024-08-05 19:55:36 +0100

    Update file data

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-  "example.com/price-calculator-go/prices"
+	"example.com/price-calculator/prices"
 )
 
-func main(){
- var taxRates: []float{0,0.07,0.1,0.15}
+func main() {
+	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
- for _, taxRate := range taxRates {
-   priceJob := prices.NewTaxIncludedPriceJob(taxRate)
-   priceJob.Process()
- }
+	for _, taxRate := range taxRates {
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
+	}
 
- fmt.Println(result)
 }
--- prices/prices.go
+++ prices/prices.go
@@ -1,56 +1,78 @@
 package prices
 
 import (
-  "fmt"
-  "os"
-  "bufio"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
 )
 
-type TaxInclusedPriceJob struct {
-  TaxRate float64
-  InputPrices []float64
-  TaxIncludedPrices map[string]float64
+type TaxIncludedPriceJob struct {
+	TaxRate           float64
+	InputPrices       []float64
+	TaxIncludedPrices map[string]float64
 }
 
-func (job TaxIncludedPriceJob) LoadData(){
-  file, err := os.Open("prices.txt")
+func (job *TaxIncludedPriceJob) LoadData() {
+	file, err := os.Open("prices.txt")
 
-  if err != nil {
-    fmt.Println("Could not open file")
-    fmt.Println(err)
-    return
-  }
+	if err != nil {
+		fmt.Println("Could not open file!")
+		fmt.Println(err)
+		return
+	}
 
-  scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(file)
 
-  var lines []string
-  for scanner.Scan() {
-    lines = append(lines, scanner.Text())
-  }
+	var lines []string
 
-  err := scanner.Err()
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
 
-  if err != nil {
-    fmt.Println("Reading the file content failed")
-    fmt.Println(err)
-    file.Close()
-    return 
-  }
+	err = scanner.Err()
+
+	if err != nil {
+		fmt.Println("Reading the file content failed.")
+		fmt.Println(err)
+		file.Close()
+		return
+	}
+
+	prices := make([]float64, len(lines))
+
+	for lineIndex, line := range lines {
+		floatPrice, err := strconv.ParseFloat(line, 64)
+
+		if err != nil {
+			fmt.Println("Converting price to float failed.")
+			fmt.Println(err)
+			file.Close()
+			return
+		}
+
+		prices[lineIndex] = floatPrice
+	}
+
+	job.InputPrices = prices
 }
 
-func (job TaxIncludedPriceJob) Process(){
-  result = make(map[string]float64)
+func (job *TaxIncludedPriceJob) Process() {
+	job.LoadData()
 
-  for priceIndex, price := range job.InputPrices {
-    result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
-  }
+	result := make(map[string]string)
 
-  fmt.Println(result)
+	for _, price := range job.InputPrices {
+		taxIncludedPrice := price * (1 + job.TaxRate)
+		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.2f", taxIncludedPrice)
+	}
+
+	fmt.Println(result)
 }
 
-func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludesPriceJob {
-  return &TaxIncludedPriceJob {
-    InputPrices: []float64{10,20,30},
-    TaxRate: taxRate
-  }
+func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
+	return &TaxIncludedPriceJob{
+		InputPrices: []float64{10, 20, 30},
+		TaxRate:     taxRate,
+	}
 }
