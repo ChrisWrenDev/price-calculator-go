c9de8b97cd872fc84e0e5c48931cb4eb94386bdb
Chris Wren <chris.wren@bt.com>
2024-08-05 08:26:29 +0100

    Basic version

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
 func main(){
+ prices := []float{10,20,30}
+ var taxRates: []float{0,0.07,0.1,0.15}
 
+ var result map[float64][]float64
+
+ for index, taxRate := range taxRates {
+   var taxIncludedPrices []float64 = make([]float64, len(prices))
+   for priceIndex, price := prices {
+     taxIncludedPrices[priceIndex] = price * (1 + taxRate)
+   }
+   result[taxRate] = taxIncludedPrices
+ }
+
+ fmt.Println(result)
 }
