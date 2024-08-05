61f6e7168f93b3633f88fa671b3b3eeaac7ae3f3
Chris Wren <chris.wren@bt.com>
2024-08-05 08:47:58 +0100

    Added prices package

--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package main
 
+import (
+  "example.com/price-calculator-go/prices"
+)
+
 func main(){
- prices := []float{10,20,30}
  var taxRates: []float{0,0.07,0.1,0.15}
 
- var result map[float64][]float64
-
- for index, taxRate := range taxRates {
-   var taxIncludedPrices []float64 = make([]float64, len(prices))
-   for priceIndex, price := prices {
-     taxIncludedPrices[priceIndex] = price * (1 + taxRate)
-   }
-   result[taxRate] = taxIncludedPrices
+ for _, taxRate := range taxRates {
+   priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+   priceJob.Process()
  }
 
  fmt.Println(result)
