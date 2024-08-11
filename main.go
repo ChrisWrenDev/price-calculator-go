dbc6ae7b2600034a1d229054cfbee9db5e58a27b
Chris Wren <chris.wren@bt.com>
2024-08-11 17:56:27 +0100

    Updated to handle multiple types of managers

--- main.go
+++ main.go
@@ -3,16 +3,24 @@ package main
 import (
   "fmt"
 	"example.com/price-calculator/prices"
-  "example.com/price-calculator/fileManager"
+  "example.com/price-calculator/filemanager"
+  "example.com/price-calculator/cmdmanager"
 )
 
 func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
-    fm := fileManager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate * 100))
+    fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate * 100))
+    cmd := cmdmanager.New() 
+
     priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
-		priceJob.Process()
+    err := priceJob.Process()
+
+    if err != nil {
+      fmt.Print("Could not process job")
+      fmt.Print(err)
+    }
 	}
 
 }
